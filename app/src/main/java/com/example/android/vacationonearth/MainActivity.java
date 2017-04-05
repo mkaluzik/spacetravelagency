811df0c0f830b7bb5815dcd1b57a2305c4e5b120
mkaluzik <mkaluzik@gmail.com>
2017-04-05 21:07:00 +0200

    Send mail
    Mail intent is called when Order button is pressed

--- app/src/main/java/com/example/android/vacationonearth/MainActivity.java
+++ app/src/main/java/com/example/android/vacationonearth/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.vacationonearth;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import static android.R.id.message;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+
+
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"spacetravels@universe.scifi"});
+        intent.putExtra(Intent.EXTRA_SUBJECT,
+                "New order - Vacation on Earth");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 }
